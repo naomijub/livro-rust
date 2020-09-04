7e4a8bcd26d7174995819af3bea45216438fd7d4
Julia Naomi <jnboeira@outlook.com>
2020-09-03 21:42:51 -0300

    more examples

---
+++ p2c08/src/main.rs
@@ -0,0 +1,152 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// Número de Armstrong 
+const RADIX: u32 = 10;
+
+pub fn is_armstrong_number(num: u32) -> bool {
+    let pow = num.to_string().len() as u32;
+    num.to_string()
+      .chars()
+      .into_iter()
+      // or:  .map(|c| c.to_digit(RADIX).unwrap())
+      .filter_map(|c| c.to_digit(RADIX).ok_or("err").ok())
+      .map(|n| n.pow(pow))
+      .fold(0, |acc, n| acc + n) == num
+}
+
+#[test]
+fn armstrong_numbers() {
+    assert!(is_armstrong_number(153));
+    assert!(!is_armstrong_number(154));
+}
+
+// Isograma
+use std::collections::HashMap;
+
+pub fn check(candidate: &str) -> bool {
+    candidate
+      .to_lowercase()
+      .chars()
+      .filter(|c| c.is_alphanumeric())
+      .fold(HashMap::new(), |mut acc: HashMap<char, i32>, c| {
+        *acc.entry(c).or_insert(0) += 1;
+        acc
+        })
+      .values()
+      .fold(true,|acc,x| 
+        if x > &1 { acc && false } 
+        else { acc } 
+      )
+}
+
+#[test]
+fn isograma() {
+    assert_eq!(
+        check("six-Year-old"),
+        true,
+    );
+
+    assert_eq!(
+        check("Alphabet"),
+        false,
+    );
+}
+
+// n-ésimo primo
+
+fn is_prime(n: u32) -> bool {
+    use rayon::prelude::*;
+
+    !(2u32..n.div_euclid(2) + 1)
+        .collect::<Vec<u32>>()
+        .par_iter()
+        .any(|i| n % i == 0)
+}
+
+pub fn nth(n: u32) -> u32 {
+    match n {
+        0 => 2u32,
+        n => (1u32..)
+            .filter(|num| is_prime(*num))
+            .nth((n + 1) as usize)
+            .unwrap(),
+    }
+}
+
+#[test]
+fn test_sixth_prime() {
+    assert_eq!(nth(5), 13);
+}
+
+// #[test]
+// fn test_big_prime() {
+//     assert_eq!(nth(10_000), 104_743);
+// }
+
+// Series
+pub fn series(digits: &str, len: usize) -> Vec<String> {
+    if len <= 0 { return vec!["".to_string(); digits.len() + 1]; }
+    digits.chars()
+        .map(|c| c.to_string())
+        .collect::<Vec<String>>()
+        .windows(len)
+        .map(|v| v.join(""))
+        .collect::<Vec<String>>()
+}
+
+#[test]
+fn series_with_length_2() {
+    let expected = vec![
+        "92".to_string(),
+        "20".to_string(),
+        "01".to_string(),
+        "17".to_string(),
+    ];
+    assert_eq!(series("92017", 2), expected);
+}
+
+// Series ordem
+pub fn series_ord(digits: &str, len: usize) -> Vec<usize> {
+    if len <= 0 { return Vec::new(); }
+    let mut serie = digits.chars()
+        .map(|c| c.to_string())
+        .collect::<Vec<String>>()
+        .windows(len)
+        .map(|v| v.join(""))
+        .filter_map(|e| e.parse::<usize>().ok())
+        .collect::<Vec<usize>>();
+    serie.sort();
+    //serie.reverse();
+    serie
+}
+
+#[test]
+fn series_ord_test() {
+    let expected = vec![
+        01usize,
+        17usize,
+        20usize,
+        92usize,
+    ];
+    assert_eq!(series_ord("92017", 2), expected);
+}
+
+// Max by second series char
+pub fn max_by_second_char_series(digits: &str, len: usize) -> usize {
+    digits.chars()
+        .map(|c| c.to_string())
+        .collect::<Vec<String>>()
+        .windows(len)
+        .max_by(|a, b| a[1].parse::<usize>().unwrap_or(0).cmp(&b[1].parse::<usize>().unwrap_or(0)))
+        .unwrap()
+        .to_vec()
+        .join("")
+        .parse::<usize>().unwrap_or(0)
+}
+
+#[test]
+fn max_by_series_test() {
+    assert_eq!(max_by_second_char_series("92017", 2), 17usize);
+}
